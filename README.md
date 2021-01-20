c2cda2c2e1d7d57df5ae240c5939b38dd056e588
cschuhknecht <cs@schuhknechts.de>
2021-01-20 11:19:25 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# DiveraESP
+Trigger a DIVERA 24/7 alarm via an ESP8266 if your Pager receives an alarm.
+
+![](images/main_image.jpg)
+
+## About DIVERA 24/7
+DIVERA 24/7 (https://www.divera247.com/) is an alerting and availability system. It uses Hardware to detect if one of the pagers of the fire department receives an alarm. If so this Hardware sends a request to the DIVERA REST API that then Triggers a Push notification on linked mobile phones via an app.
+It also keeps track of the availability of first responders.
+The Mentioned API can easily be accessed. How? Read the documentation pdf in the documentation folder.
+
+## About DiveraESP
+DiveraESP is basically Code that runs on an ESP8266 (for example a WeMos D1 mini) that gets its power via the USB Port of the Pager (if it has one, this is tested with the shown Airbus model) if it receives an alarm. The ESP than interacts with the API and triggers the alert on the mobile phone(s).
+
+There for you need a DIVERA account, a phone, the app and an ESP8266 like the mentioned WeMos one. The documentation tells you everything you need to know about the URLs the ESP8266 is going to send a GET request to.
+
+The URL and the Wi-Fi login has to be changed in the Code in Order to work.
+
+As you can already tell this solution is not perfect because you can't know any details about the alert. You only will know that there is an alert. I work on that.
+
+The DIVERA setup isn't covered here, but I will explain how to flash the code on your ESP8266.
+
+## Dependencies / How to flash
+To flash the code on your ESP you need the Arduino IDE (https://www.arduino.cc/en/software). In the IDE you have to install a couple of things.
+
+### Board Manager
+You need to install the ESP8266 Board Manager. To do this, go to *File/Preferences* in the IDE and paste the following URL into the board manager URL line. http://arduino.esp8266.com/stable/package_esp8266com_index.json Then go to *Tools/Board: ...* and at the top on *Board Manager...* Search for esp8266 by ESP8266 Community and install the newest version.  The last step is to select the board under *Tools/Board*. For example *LOLIN(WEMOS) D1 R2 & mini* if you use a WeMos D1 mini. If you have problems with the code. Try to use esp8266 in Version 2.7.4.
+
+### Libraries
+You also need to install a library.
+- NTPClient by Fabrice Weinberg (tested with version 3.2.0)
+
+Here is how to install them. Go to *Tools/Manage Libraries...* and search for the Libraries. Then click install.
+
+### Flash it
+If you selected your board like mentioned above then all there's left is to choose the USB Port the ESP is connected to. You can do that by Tools/Port. Then flash it via the arrow button at the top. Done.
+
+## Future changes
+- divide the code with classes
+- put it all in setup() instead of loop()
+- improve the time code
---
+++ code/divera_esp.ino
@@ -0,0 +1,123 @@
+#include <ESP8266WiFi.h> //library provides ESP8266 specific WiFi methods we are calling to connect to network
+#include <ESP8266HTTPClient.h> //library provides ESP8266 specific http methods
+//#include <WiFiClient.h> // additional and unused Wifi Library
+#include <WiFiUdp.h>
+#include <NTPClient.h>
+
+const char* ssid = "xxx"; // your wifi ssid
+const char* password = "xxx"; // your wifi password
+
+//Your Domain name with URL path or IP address with path (use http instead of https)
+const char* serverName = "http://www.divera247.com/api/xxx";
+
+// Zeitkram
+const long utcOffsetInSeconds = 3600;
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+// Define NTP Client to get time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", utcOffsetInSeconds);
+
+// The setup runs only ones. Its used for wifi connection.
+void setup() {
+  Serial.begin(115200); // initialize serial (text) output
+
+  WiFi.begin(ssid, password); // connect to wifi
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) { // wait for wifi connection
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  timeClient.begin();
+}
+
+// The loop runs multiple times. But I blocked that with a huge delay at the end.
+void loop() {
+  timeClient.update();
+
+  unsigned long epochTime = timeClient.getEpochTime();
+  struct tm *ptm = gmtime ((time_t *)&epochTime);
+
+  int monthDay = ptm->tm_mday;
+
+  int daylightSaving = ptm->tm_isdst;
+
+  int day = timeClient.getDay();
+  int hour = timeClient.getHours()+daylightSaving;
+  int minute = timeClient.getMinutes();
+
+  String httpRequestData;
+  if (monthDay >= 1 && monthDay <=7 && day == 0 && hour == 12 && minute >= 25 && minute <= 35) {
+    httpRequestData = "type=Probealarm&priority=0";
+  }
+  else {
+    httpRequestData = "type=Einsatz";
+  }
+
+  bool printInfo = true;
+  if (printInfo==true){
+    Serial.print("> ");
+    Serial.print(daysOfTheWeek[day]);
+    Serial.print(" (");
+    Serial.print(day);
+    Serial.print("), ");
+    Serial.print(hour);
+    Serial.print(":");
+    Serial.print(minute);
+    Serial.print(":");
+    Serial.print(timeClient.getSeconds());
+    Serial.print(", ");
+
+    Serial.print("Month day: ");
+    Serial.print(monthDay);
+    Serial.print(", ");
+
+    Serial.print("Daylight Saving: ");
+    Serial.print(daylightSaving);
+  }
+
+  Serial.print(", Alarmtyp: ");
+  Serial.println(httpRequestData);
+
+
+  //Check WiFi connection status
+  if(WiFi.status()== WL_CONNECTED){ // Ask if wifi is still connected because if not the following would be unnessesary
+    HTTPClient http; // initialize http client with the simple name http
+
+    // Your Domain name with URL path or IP address with path
+    http.begin(serverName);
+
+    // Specify content-type header
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    // Data to send with HTTP POST
+    //String httpRequestData = "type=F1+Dachgeschoss&text=Person+im+Gebeaude%2C";
+    // Send HTTP POST request
+    int httpResponseCode = http.POST(httpRequestData);
+    // Get http payload
+    String payload = http.getString();
+    // print response and payload
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    Serial.print("Payload: ");
+    Serial.println(payload);
+
+    // Free resources
+    http.end();
+
+    // If you need an HTTP request with a content type: application/json, use the following:
+    //http.addHeader("Content-Type", "application/json");
+    //int httpResponseCode = http.POST("{\"api_key\":\"tPmAT5Ab3j7F9\",\"sensor\":\"BME280\",\"value1\":\"24.25\",\"value2\":\"49.54\",\"value3\":\"1005.14\"}");
+
+    // If you need an HTTP request with a content type: text/plain
+    //http.addHeader("Content-Type", "text/plain");
+    //int httpResponseCode = http.POST("Hello, World!");
+  }
+  else {
+    Serial.println("WiFi Disconnected");
+  }
+  delay(300000); // huge delay - blocks loop
+
+}
